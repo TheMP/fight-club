299fb3811c676dd7d78f452b2eb026ecb9bdd894
Andrzej Skrodki <andrzej.skrodzki@gmail.com>
2019-11-17 09:25:30 +0200

    fixes.

--- app/src/pages/courses/Burnout.tsx
+++ app/src/pages/courses/Burnout.tsx
@@ -16,7 +16,6 @@ export default class BurnoutCourse extends React.Component<NavigationContainerPr
       <View style={[styles.page, { flex: 1, paddingTop: 30 }]}>
         <Class category="Burnout" title="Introduction to burnout" icon="book" completed={true} />
         <Class category="Burnout" title="Burnout: Symptoms and indicators" icon="book" completed={false} />
-        <Class category="Burnout" title="Burnout: Symptoms and indicators" icon="book" completed={false} />
         <TouchableHighlight onPress={() => this.props.navigation!.navigate('Chat', { which: 1 })} >
           <Class
             category="Burnout"
